32c15d2af6fea25f3aec96303c33375c73a7d111
mpochard <88907375+m3L4n@users.noreply.github.com>
2024-12-02 00:00:39 +0100

    chore : clean of to file

--- pkg/utils/osFileToTensor.go
+++ pkg/utils/osFileToTensor.go
@@ -71,41 +71,6 @@ func ZScoreNormTensor(tensor t.Tensor) t.Tensor {
 	return normTensor
 }
 
-func OsFileToTensor(dataset *os.File) (t.Tensor, t.Tensor, t.Tensor, t.Tensor) {
-	noHeader := dataframe.HasHeader(false)
-	var nameColumn = []string{"ID", "Diagnosis", "radius", "texture", "perimeter", "area", "smoothness", "compactness", "concavity", "concave points", "symmetry", "fractal dimension"}
-	i := 0
-	for len(nameColumn) < 32 {
-
-		nameColumn = append(nameColumn, "f"+strconv.Itoa(i))
-		i++
-	}
-	namesColumn := dataframe.Names(nameColumn...)
-	var df dataframe.DataFrame = dataframe.ReadCSV(dataset, namesColumn, noHeader)
-	dfTrain, dfTest, errTTS := data.TrainTestSplit(df, 0.15, false)
-	handleError(errTTS)
-
-	var yTrain = dfTrain.Select("Diagnosis")
-	var yTest = dfTest.Select("Diagnosis")
-	var xTrain = dfTrain.Drop([]string{"ID", "Diagnosis", "radius", "perimeter", "area", "f10", "f12", "f13"})
-	// var xTrain = dfTrain.Drop([]string{"ID", "Diagnosis", })
-	// var xTest = dfTest.Drop([]string{"ID", "Diagnosis" })
-	var xTest = dfTest.Drop([]string{"ID", "Diagnosis", "radius", "perimeter", "area", "f10", "f12", "f13"})
-	// var xTest = dfTest.Drop([]string{"ID", "Diagnosis",  "perimeter", "area", "concave points", "f10", "f12", "f13"})
-	xTrainTensor, err := DfToTensorFloat64(xTrain)
-	handleError(err)
-	xTestTenosr, err := DfToTensorFloat64(xTest)
-	handleError(err)
-	yTrainTensor, err := DfToTensorLabel(yTrain)
-	handleError(err)
-	yTestTensor, err := DfToTensorLabel(yTest)
-	handleError(err)
-	normXTrain := ZScoreNormTensor(xTrainTensor)
-	// normXTrain := data.NormTensor(xTrainTensor)
-	// normXTest := data.NormTensor(xTestTenosr)
-	normXTest := ZScoreNormTensor(xTestTenosr)
-	return normXTrain, yTrainTensor, normXTest, yTestTensor
-}
 
 func osToDF(dataset *os.File) dataframe.DataFrame {
 	noHeader := dataframe.HasHeader(false)
